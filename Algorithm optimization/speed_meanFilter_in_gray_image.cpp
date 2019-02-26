34220d16e9f11b301e79ecfd948f465a0c118f47
BBuf <1182563586@qq.com>
2019-02-26 16:37:15 +0800

    Update speed_meanFilter_in_gray_image.cpp openmp

--- Algorithm optimization/speed_meanFilter_in_gray_image.cpp
+++ Algorithm optimization/speed_meanFilter_in_gray_image.cpp
@@ -1,3 +1,8 @@
+#include <stdio.h>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+using namespace std;
 //所有代码针对灰度图，RGB分为3个通道处理
 //中值滤波串行代码
 Mat SerialMedianFiltering(Mat src) {
@@ -91,6 +96,85 @@ Mat x86MedianFiltering(Mat src, const int BX, const int BY) {
 	for (int i = 0; i < col; i++) {
 		dst.at<uchar>(0, i) = src.at<uchar>(0, i);
 		dst.at<uchar>(row - 1, i) = src.at<uchar>(row - 1, i);
+	}
+	return dst;
+}
+
+//openmp + x86循环展开优化均值滤波
+Mat OpenmpMedianFiltering(Mat src, const int BX, const int BY) {
+	int row = src.rows;
+	int col = src.cols;
+	Mat dst(row, col, CV_8UC1);
+	int i, j;
+#pragma omp parallel for lastprivate(i)
+	for (i = 1; i < row - 1 - BY; i += BY) {
+		for (j = 1; j < col - 1 - BX; j += BX) {
+			unsigned short *temp = new unsigned short[BX*BY];
+			memset(temp, 0, sizeof(temp));
+			for (int ii = 0; ii < BY; ii++) {
+				for (int jj = 0; jj < BX; jj++) {
+					temp[ii*BX + jj] += src.at<uchar>(i + ii, j + jj - 1);
+					temp[ii*BX + jj] += src.at<uchar>(i + ii, j + jj);
+					temp[ii*BX + jj] += src.at<uchar>(i + ii, j + jj + 1);
+					temp[ii*BX + jj] += src.at<uchar>(i + ii + 1, j + jj - 1);
+					temp[ii*BX + jj] += src.at<uchar>(i + ii + 1, j + jj);
+					temp[ii*BX + jj] += src.at<uchar>(i + ii + 1, j + jj + 1);
+					temp[ii*BX + jj] += src.at<uchar>(i + ii - 1, j + jj - 1);
+					temp[ii*BX + jj] += src.at<uchar>(i + ii - 1, j + jj);
+					temp[ii*BX + jj] += src.at<uchar>(i + ii - 1, j + jj + 1);
+				}
+			}
+			for (int ii = 0; ii < BY; ii++) {
+				for (int jj = 0; jj < BX; jj++) {
+					dst.at<uchar>(i + ii, j + jj) = (unsigned char)(temp[ii * BX + jj] / 9);
+				}
+			}
+		}
+		for (int ii = 0; ii < BY; ii++) {
+			for (int je = j; je < col - 1; je++) {
+				unsigned short temp = 0;
+				temp += src.at<uchar>(i + ii, je);
+				temp += src.at<uchar>(i + ii, je + 1);
+				temp += src.at<uchar>(i + ii, je - 1);
+
+				temp += src.at<uchar>(i + ii + 1, je);
+				temp += src.at<uchar>(i + ii + 1, je + 1);
+				temp += src.at<uchar>(i + ii + 1, je - 1);
+
+				temp += src.at<uchar>(i + ii - 1, je);
+				temp += src.at<uchar>(i + ii - 1, je + 1);
+				temp += src.at<uchar>(i + ii - 1, je - 1);
+				dst.at<uchar>(i + ii, je) = (unsigned char)(temp / 9);
+			}
+		}
+	}
+	for (int ie = i; ie < row - 1; ie++) {
+		for (int j = 1; j < col - 1; j++) {
+			unsigned short temp = 0;
+			temp += src.at<uchar>(ie, j);
+			temp += src.at<uchar>(ie, j + 1);
+			temp += src.at<uchar>(ie, j - 1);
+			temp += src.at<uchar>(ie - 1, j);
+			temp += src.at<uchar>(ie - 1, j + 1);
+			temp += src.at<uchar>(ie - 1, j - 1);
+			temp += src.at<uchar>(ie + 1, j);
+			temp += src.at<uchar>(ie + 1, j + 1);
+			temp += src.at<uchar>(ie + 1, j - 1);
+			dst.at<uchar>(ie, j) = (unsigned char)(temp / 9);
+		}
+	}
+#pragma omp parallel
+	{
+#pragma omp for
+		for (int i = 0; i < row; i++) {
+			dst.at<uchar>(i, 0) = src.at<uchar>(i, 0);
+			dst.at<uchar>(i, col - 1) = src.at<uchar>(i, col - 1);
+		}
+#pragma omp for
+		for (int i = 0; i < col; i++) {
+			dst.at<uchar>(0, i) = src.at<uchar>(0, i);
+			dst.at<uchar>(row - 1, i) = src.at<uchar>(row - 1, i);
+		}
 	}
 	return dst;
 }
