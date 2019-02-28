04355f544bea45c178ca5a9f884936b236b9a83e
BBuf <1182563586@qq.com>
2019-02-28 21:14:39 +0800

    Update Huang_Fast_MedianBlur.cpp

--- Algorithm optimization/Huang_Fast_MedianBlur.cpp
+++ Algorithm optimization/Huang_Fast_MedianBlur.cpp
@@ -1,10 +1,3 @@
-#include <stdio.h>
-#include <iostream>
-#include <immintrin.h>
-#include <opencv2/opencv.hpp>
-using namespace cv;
-using namespace std;
-
 //计算中值
 int getMediaValue(const int hist[], int thresh) {
 	int sum = 0;
@@ -17,7 +10,6 @@ int getMediaValue(const int hist[], int thresh) {
 	return 255;
 }
 //快速中值滤波，灰度图
-//https://www.cnblogs.com/liulijin/p/9038230.html
 Mat fastMedianBlur(Mat src, int diameter) {
 	int row = src.rows;
 	int col = src.cols;
@@ -31,6 +23,47 @@ Mat fastMedianBlur(Mat src, int diameter) {
 	int right = col - radius;
 	int bot = row - radius;
 	for (int j = radius; j < bot; j++) {
-
+		for (int i = radius; i < right; i++) {
+			//每一行第一个待滤波元素建立直方图
+			if (i == radius) {
+				memset(Hist, 0, sizeof(Hist));
+				for (int y = j - radius; y <= min(j + radius, row); y++) {
+					for (int x = i - radius; x <= min(i + radius, col); x++) {
+						uchar val = srcData[y * col + x];
+						Hist[val]++;
+					}
+				}
+			}
+			else {
+				int L = i - radius - 1;
+				int R = i + radius;
+				for (int y = j - radius; y <= min(j + radius, row); y++) {
+					//更新左边一列
+					Hist[srcData[y * col + L]]--;
+					//更新右边一列
+					Hist[srcData[y * col + R]]++;
+				}
+			}
+			uchar medianVal = getMediaValue(Hist, threshold);
+			dstData[j * col + i] = medianVal;
+		}
 	}
+	//边界直接赋值
+	for (int i = 0; i < col; i++) {
+		for (int j = 0; j < radius; j++) {
+			int id1 = j * col + i;
+			int id2 = (row - j - 1) * col + i;
+			dstData[id1] = srcData[id1];
+			dstData[id2] = srcData[id2];
+		}
+	}
+	for (int i = radius; i < row - radius - 1; i++) {
+		for (int j = 0; j < radius; j++) {
+			int id1 = i * col + j;
+			int id2 = i * col + col - j - 1;
+			dstData[id1] = srcData[id1];
+			dstData[id2] = srcData[id2];
+		}
+	}
+	return dst;
 }
