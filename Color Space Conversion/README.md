104fa6cc0c36887e8a982a3f0cfeae3dd2b2976d
BBuf <1182563586@qq.com>
2019-04-09 14:56:13 +0800

    Add RGBYCbCr.cpp

--- Color Space Conversion/README.md
+++ Color Space Conversion/README.md
@@ -3,3 +3,4 @@
 - RGBYUV.cpp C++实现RGB和YUV色彩空间相互转换算法。原理请看：https://blog.csdn.net/just_sort/article/details/87102898
 - RGBHSV.cpp C++实现RGB和HSV色彩空间相互转换算法。原理请看：https://blog.csdn.net/just_sort/article/details/87102898
 - RGBHSI.cpp C++实现RGB和HSI色彩空间相互转换算法。原理请看：https://blog.csdn.net/just_sort/article/details/87102898
+- RGBYCbCr.cpp C++实现了RGB和YCbCr色彩空间的相互转换算法。原理请看：https://blog.csdn.net/just_sort/article/details/87102898
---
+++ Color Space Conversion/RGBYCbCr.cpp
@@ -0,0 +1,82 @@
+const float YCbCrYRF = 0.299F;              // RGB转YCbCr的系数(浮点类型）
+const float YCbCrYGF = 0.587F;
+const float YCbCrYBF = 0.114F;
+const float YCbCrCbRF = -0.168736F;
+const float YCbCrCbGF = -0.331264F;
+const float YCbCrCbBF = 0.500000F;
+const float YCbCrCrRF = 0.500000F;
+const float YCbCrCrGF = -0.418688F;
+const float YCbCrCrBF = -0.081312F;
+
+const float RGBRYF = 1.00000F;            // YCbCr转RGB的系数(浮点类型）
+const float RGBRCbF = 0.0000F;
+const float RGBRCrF = 1.40200F;
+const float RGBGYF = 1.00000F;
+const float RGBGCbF = -0.34414F;
+const float RGBGCrF = -0.71414F;
+const float RGBBYF = 1.00000F;
+const float RGBBCbF = 1.77200F;
+const float RGBBCrF = 0.00000F;
+
+const int Shift = 20;
+const int HalfShiftValue = 1 << (Shift - 1);
+
+const int YCbCrYRI = (int)(YCbCrYRF * (1 << Shift) + 0.5);         // RGB转YCbCr的系数(整数类型）
+const int YCbCrYGI = (int)(YCbCrYGF * (1 << Shift) + 0.5);
+const int YCbCrYBI = (int)(YCbCrYBF * (1 << Shift) + 0.5);
+const int YCbCrCbRI = (int)(YCbCrCbRF * (1 << Shift) + 0.5);
+const int YCbCrCbGI = (int)(YCbCrCbGF * (1 << Shift) + 0.5);
+const int YCbCrCbBI = (int)(YCbCrCbBF * (1 << Shift) + 0.5);
+const int YCbCrCrRI = (int)(YCbCrCrRF * (1 << Shift) + 0.5);
+const int YCbCrCrGI = (int)(YCbCrCrGF * (1 << Shift) + 0.5);
+const int YCbCrCrBI = (int)(YCbCrCrBF * (1 << Shift) + 0.5);
+
+const int RGBRYI = (int)(RGBRYF * (1 << Shift) + 0.5);              // YCbCr转RGB的系数(整数类型）
+const int RGBRCbI = (int)(RGBRCbF * (1 << Shift) + 0.5);
+const int RGBRCrI = (int)(RGBRCrF * (1 << Shift) + 0.5);
+const int RGBGYI = (int)(RGBGYF * (1 << Shift) + 0.5);
+const int RGBGCbI = (int)(RGBGCbF * (1 << Shift) + 0.5);
+const int RGBGCrI = (int)(RGBGCrF * (1 << Shift) + 0.5);
+const int RGBBYI = (int)(RGBBYF * (1 << Shift) + 0.5);
+const int RGBBCbI = (int)(RGBBCbF * (1 << Shift) + 0.5);
+const int RGBBCrI = (int)(RGBBCrF * (1 << Shift) + 0.5);
+
+Mat RGB2YCbCr(Mat src) {
+	int row = src.rows;
+	int col = src.cols;
+	Mat dst(row, col, CV_8UC3);
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
+			int Blue = src.at<Vec3b>(i, j)[0];
+			int Green = src.at<Vec3b>(i, j)[1];
+			int Red = src.at<Vec3b>(i, j)[2];
+			dst.at<Vec3b>(i, j)[0] = (int)((YCbCrYRI * Red + YCbCrYGI * Green + YCbCrYBI * Blue + HalfShiftValue) >> Shift);
+			dst.at<Vec3b>(i, j)[1] = (int)(128 + ((YCbCrCbRI * Red + YCbCrCbGI * Green + YCbCrCbBI * Blue + HalfShiftValue) >> Shift));
+			dst.at<Vec3b>(i, j)[2] = (int)(128 + ((YCbCrCrRI * Red + YCbCrCrGI * Green + YCbCrCrBI * Blue + HalfShiftValue) >> Shift));
+		}
+	}
+	return dst;
+}
+
+Mat YCbCr2RGB(Mat src) {
+	int row = src.rows;
+	int col = src.cols;
+	Mat dst(row, col, CV_8UC3);
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
+			int Y = src.at<Vec3b>(i, j)[0];
+			int Cb = src.at<Vec3b>(i, j)[1] - 128;
+			int Cr = src.at<Vec3b>(i, j)[2] - 128;
+			int Red = Y + ((RGBRCrI * Cr + HalfShiftValue) >> Shift);
+			int Green = Y + ((RGBGCbI * Cb + RGBGCrI * Cr + HalfShiftValue) >> Shift);
+			int Blue = Y + ((RGBBCbI * Cb + HalfShiftValue) >> Shift);
+			if (Red > 255) Red = 255; else if (Red < 0) Red = 0;
+			if (Green > 255) Green = 255; else if (Green < 0) Green = 0;    // 编译后应该比三目运算符的效率高
+			if (Blue > 255) Blue = 255; else if (Blue < 0) Blue = 0;
+			dst.at<Vec3b>(i, j)[0] = Blue;
+			dst.at<Vec3b>(i, j)[1] = Green;
+			dst.at<Vec3b>(i, j)[2] = Red;
+		}
+	}
+	return dst;
+}
