839cf518f5caab2f805fb28f27896c872b54287c
BBuf <1182563586@qq.com>
2019-04-15 14:24:56 +0800

    Add BilinearInterpolation.cpp

---
+++ Image Interpolation/BilinearInterpolation.cpp
@@ -0,0 +1,30 @@
+//双线性插值
+Mat BilinearInterpolation(Mat src, float sx, float sy) {
+	int row = src.rows;
+	int col = src.cols;
+	int channels = src.channels();
+	int dst_row = round(row * sx);
+	int dst_col = round(col * sy);
+	Mat dst(dst_row, dst_col, CV_8UC3);
+	for (int i = 0; i < dst_row; i++) {
+		int index_i = (i + 0.5) / sx - 0.5;
+		if (index_i < 0) index_i = 0;
+		if (index_i > row - 1) index_i = row - 1;
+		int i1 = floor(index_i);
+		int i2 = ceil(index_i);
+		float u = index_i - i;
+		for (int j = 0; j < dst_col; j++) {
+			float index_j = (j + 0.5) / sy - 0.5;
+			if (index_j < 0) index_j = 0;
+			if (index_j > col - 1) index_j = col - 1;
+			int j1 = floor(index_j);
+			int j2 = ceil(index_j);
+			float v = index_j - j1;
+			for (int k = 0; k < 3; k++) {
+				dst.at<cv::Vec3b>(i, j)[k] = (1 - u)*(1 - v)*src.at<cv::Vec3b>(i1, j1)[k] + 
+					(1 - u)*v*src.at<cv::Vec3b>(i1, j2)[k] + u*(1 - v)*src.at<cv::Vec3b>(i2, j1)[k] + u*v*src.at<cv::Vec3b>(i2, j2)[k];
+			}
+		}
+	}
+	return dst;
+}
--- Image Interpolation/README.md
+++ Image Interpolation/README.md
@@ -1,3 +1,4 @@
 # OpenCV和C++实现图像处理中各种插值算法
 
 - NearestNeighborInterpolation.cpp C++实现了OpenCV中的最常见的邻近插值。
+- BilinearInterpolation.cpp C++实现了OpenCV中的双线性插值，精度比较高，原理请看：https://handspeaker.iteye.com/blog/1545126
