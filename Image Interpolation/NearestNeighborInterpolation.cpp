fb64f91c2d8e94788fd5432983055693104bf675
Xiaoyu Zhang <35585791+BBuf@users.noreply.github.com>
2021-06-29 21:50:14 +0800

    Update NearestNeighborInterpolation.cpp

--- Image Interpolation/NearestNeighborInterpolation.cpp
+++ Image Interpolation/NearestNeighborInterpolation.cpp
@@ -9,8 +9,8 @@ Mat NearestNeighborInterpolation(Mat src, float sx, float sy) {
 		Mat dst(dst_row, dst_col, CV_8UC1);
 		for (int i = 0; i < dst_row; i++) {
 			for (int j = 0; j < dst_col; j++) {
-				int pre_i = round(i / sy);
-				int pre_j = round(j / sx);
+				int pre_i = floor(i / sy);
+				int pre_j = floor(j / sx);
 				if (pre_i > row - 1) pre_i = row - 1;
 				if (pre_j > col - 1) pre_j = col - 1;
 				dst.at<uchar>(i, j) = src.at<uchar>(pre_i, pre_j);
@@ -22,8 +22,8 @@ Mat NearestNeighborInterpolation(Mat src, float sx, float sy) {
 		Mat dst(dst_row, dst_col, CV_8UC3);
 		for (int i = 0; i < dst_row; i++) {
 			for (int j = 0; j < dst_col; j++) {
-				int pre_i = round(i / sy);
-				int pre_j = round(j / sx);
+				int pre_i = floor(i / sy);
+				int pre_j = floor(j / sx);
 				if (pre_i > row - 1) pre_i = row - 1;
 				if (pre_j > col - 1) pre_j = col - 1;
 				dst.at<Vec3b>(i, j)[0] = src.at<Vec3b>(pre_i, pre_j)[0];
